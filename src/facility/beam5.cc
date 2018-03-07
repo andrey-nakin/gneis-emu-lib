6d6f2e543230b3ae10b9632ea36b4f117441bce4
Andrey Nakin <andrey@ter-net.com>
2018-03-07 18:47:54 +0300

    work

--- src/facility/beam5.cc
+++ src/facility/beam5.cc
@@ -6,6 +6,7 @@
 #include <G4Tubs.hh>
 #include <G4Sphere.hh>
 #include <G4Trd.hh>
+#include <G4SubtractionSolid.hh>
 #include <G4LogicalVolume.hh>
 #include <G4PVPlacement.hh>
 #include <G4SystemOfUnits.hh>
@@ -30,31 +31,65 @@ G4VPhysicalVolume* gneis::facility::Beam5::Construct() {
 	const G4bool checkOverlaps = true;
 	const G4double worldRadius = 100.0 * mm;
 
-	const auto solidWorld = new G4Box("World", 100 * mm, 100 * mm, 16 * m);
+	const G4String nameWorld = "world";
+	const auto solidWorld = new G4Tubs(nameWorld, 0.0, worldRadius, 18.0 * m,
+			0.0 * deg, 360.0 * deg);
 
 	const auto logicWorld = new G4LogicalVolume(solidWorld,	//	reference to Solid
 			nist->FindOrBuildMaterial("G4_Galactic"),	//	world material
-			"World");	//	name
+			nameWorld);	//	name
 	logicWorld->SetVisAttributes(G4VisAttributes::Invisible);
 
 	const auto physWorld = new G4PVPlacement(0,      //no rotation
 			G4ThreeVector(),       //at (0,0,0)
 			logicWorld,            //its logical volume
-			"World",               //its name
+			nameWorld,               //its name
 			0,                     //its mother  volume
 			false,                 //no boolean operation
 			0,                     //copy number
 			checkOverlaps);        //overlaps checking
 
-	const G4String strFinalCollimator = "collimator-C5";
-	const auto solidFinalCollimator = new G4Tubs(strFinalCollimator,
-			0.5 * toDouble(diameter),	// min radius
-			worldRadius,	// max radius
-			1000.0 * mm,	// length
+	// Collimator C1
+	const G4String strC1 = "collimator-C1";
+	const auto solidC1Whole = new G4Tubs(strC1, 0.0, worldRadius, 200.0 * mm,
+			0.0 * deg, 360.0 * deg);
+	const auto solidC1Cut = new G4Box(strC1, 70.0 * mm, 90.0 * mm, 200.0 * m);
+	const auto solidC1 = new G4SubtractionSolid(strC1, solidC1Whole,
+			solidC1Cut);
+	const auto logicC1 = new G4LogicalVolume(solidC1,
+			nist->FindOrBuildMaterial("G4_BRASS"), strC1);
+
+	// Collimator C2
+	const G4String strC2 = "collimator-C2";
+	const auto solidC2Whole = new G4Tubs(strC2, 0.0, worldRadius, 355.0 * mm,
+			0.0 * deg, 360.0 * deg);
+	const auto solidC2Cut = new G4Box(strC2, 105.0 * mm, 50.0 * mm, 355.0 * m);
+	const auto solidC2 = new G4SubtractionSolid(strC2, solidC2Whole,
+			solidC2Cut);
+	const auto logicC2 = new G4LogicalVolume(solidC2,
+			nist->FindOrBuildMaterial("G4_STEEL"), strC2);
+
+	// Collimator C3
+	const G4String strC3 = "collimator-C3";
+	const auto solidC3 = new G4Tubs(strC3, 60.0 * mm, worldRadius, 300.0 * mm, 0.0 * deg, 360.0 * deg);
+	const auto logicC3 = new G4LogicalVolume(solidC3,
+			nist->FindOrBuildMaterial("G4_STEEL"), strC3);
+
+	// Collimator C4
+	const G4String strC4 = "collimator-C4";
+	const auto solidC4 = new G4Tubs(strC4, 60.0 * mm, worldRadius, 0.5 * 875.0 * mm, 0.0 * deg, 360.0 * deg);
+	const auto logicC4 = new G4LogicalVolume(solidC4,
+			nist->FindOrBuildMaterial("G4_STEEL"), strC4);
+
+	// Collimator C5
+	const G4String strC5 = "collimator-C5";
+	const auto solidC5 = new G4Tubs(strC5, 0.5 * toDouble(diameter), // min radius
+	worldRadius,	// max radius
+			500.0 * mm,	// length
 			0.0 * deg,	// start angle
 			360.0 * deg);	// end angle
-	const auto logicFinalCollimator = new G4LogicalVolume(solidFinalCollimator,
-			nist->FindOrBuildMaterial("G4_BRASS"), strFinalCollimator);
+	const auto logicC5 = new G4LogicalVolume(solidC5,
+			nist->FindOrBuildMaterial("G4_BRASS"), strC5);
 
 	return physWorld;
 }
