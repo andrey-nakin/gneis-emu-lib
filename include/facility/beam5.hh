71fca315842147db7bd1b75ce8730df6a9f6b094
Andrey Nakin <andrey@ter-net.com>
2018-03-07 17:45:43 +0300

    work

--- include/facility/beam5.hh
+++ include/facility/beam5.hh
@@ -10,14 +10,21 @@ namespace facility {
 class Beam5: public G4VUserDetectorConstruction {
 public:
 
-	Beam5(G4VSensitiveDetector* aDetector);
+	typedef enum {
+		D50, D75, D100
+	} CollimatorDiameter;
+
+	Beam5(CollimatorDiameter aDiameter, G4VSensitiveDetector* aDetector);
 	virtual ~Beam5();
 
 	virtual G4VPhysicalVolume* Construct();
 
 private:
 
+	CollimatorDiameter const diameter;
 	G4VSensitiveDetector* const detector;
+
+	static G4double toDouble(CollimatorDiameter d);
 
 };
 
--- src/facility/beam5.cc
+++ src/facility/beam5.cc
@@ -3,6 +3,7 @@
 #include <G4Box.hh>
 #include <G4Cons.hh>
 #include <G4Orb.hh>
+#include <G4Tubs.hh>
 #include <G4Sphere.hh>
 #include <G4Trd.hh>
 #include <G4LogicalVolume.hh>
@@ -13,8 +14,9 @@
 
 #include "facility/beam5.hh"
 
-gneis::facility::Beam5::Beam5(G4VSensitiveDetector* const aDetector) :
-		G4VUserDetectorConstruction(), detector(aDetector) {
+gneis::facility::Beam5::Beam5(CollimatorDiameter const aDiameter,
+		G4VSensitiveDetector* const aDetector) :
+		G4VUserDetectorConstruction(), diameter(aDiameter), detector(aDetector) {
 }
 
 gneis::facility::Beam5::~Beam5() {
@@ -26,6 +28,7 @@ G4VPhysicalVolume* gneis::facility::Beam5::Construct() {
 
 	// Option to switch on/off checking of volumes overlaps
 	const G4bool checkOverlaps = true;
+	const G4double worldRadius = 100.0 * mm;
 
 	const auto solidWorld = new G4Box("World", 100 * mm, 100 * mm, 16 * m);
 
@@ -43,5 +46,30 @@ G4VPhysicalVolume* gneis::facility::Beam5::Construct() {
 			0,                     //copy number
 			checkOverlaps);        //overlaps checking
 
+	const G4String strFinalCollimator = "collimator-C5";
+	const auto solidFinalCollimator = new G4Tubs(strFinalCollimator,
+			0.5 * toDouble(diameter),	// min radius
+			worldRadius,	// max radius
+			1000.0 * mm,	// length
+			0.0 * deg,	// start angle
+			360.0 * deg);	// end angle
+	const auto logicFinalCollimator = new G4LogicalVolume(solidFinalCollimator,
+			nist->FindOrBuildMaterial("G4_BRASS"), strFinalCollimator);
+
 	return physWorld;
+}
+
+G4double gneis::facility::Beam5::toDouble(CollimatorDiameter const d) {
+	switch (d) {
+	case D50:
+		return 50.0 * mm;
+
+	case D75:
+		return 75.0 * mm;
+
+	case D100:
+		return 100.0 * mm;
+	}
+
+	return 0.0;
 }
