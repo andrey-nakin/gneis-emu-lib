789de01a85d8b351f7d42bd8968203f3514037f7
Andrey Nakin <andrey@ter-net.com>
2018-04-02 21:12:48 +0300

    work

--- test/src/gneis/info/VersionTest.cc
+++ test/src/gneis/info/VersionTest.cc
@@ -2,23 +2,27 @@
 #include <gtest/gtest.h>
 #include "gneis/info/Version.hh"
 
-TEST(gneis_info_Version, Defines)
+namespace gneis {
+
+namespace info {
+
+TEST(Version, Defines)
 {
 	EXPECT_TRUE(GNEISEMULIB_VERSION_MAJOR >= 0);
 	EXPECT_TRUE(GNEISEMULIB_VERSION_MINOR >= 0);
 	EXPECT_TRUE(GNEISEMULIB_VERSION_PATCH >= 0);
 }
 
-TEST(gneis_info_Version, Get)
+TEST(Version, Get)
 {
-	const std::vector<int> ver = gneis::info::Version::Get();
+	const std::vector<int> ver = Version::Get();
 	EXPECT_EQ(3, ver.size());
 	EXPECT_EQ(GNEISEMULIB_VERSION_MAJOR, ver[0]);
 	EXPECT_EQ(GNEISEMULIB_VERSION_MINOR, ver[1]);
 	EXPECT_EQ(GNEISEMULIB_VERSION_PATCH, ver[2]);
 }
 
-TEST(gneis_info_Version, GetAsString)
+TEST(Version, GetAsString)
 {
 	std::stringstream s;
 	s
@@ -27,28 +31,32 @@ TEST(gneis_info_Version, GetAsString)
 	<< GNEISEMULIB_VERSION_MINOR
 	<< '.'
 	<< GNEISEMULIB_VERSION_PATCH;
-	const G4String ver = gneis::info::Version::GetAsString();
+	const G4String ver = Version::GetAsString();
 	EXPECT_EQ(s.str(), ver);
 }
 
-TEST(gneis_info_Version, Matches)
+TEST(Version, Matches)
 {
 	EXPECT_TRUE(
-			gneis::info::Version::Matches(GNEISEMULIB_VERSION_MAJOR, GNEISEMULIB_VERSION_MINOR, GNEISEMULIB_VERSION_PATCH)
+			Version::Matches(GNEISEMULIB_VERSION_MAJOR, GNEISEMULIB_VERSION_MINOR, GNEISEMULIB_VERSION_PATCH)
 	);
 	EXPECT_TRUE(
-			gneis::info::Version::Matches(GNEISEMULIB_VERSION_MAJOR, GNEISEMULIB_VERSION_MINOR)
+			Version::Matches(GNEISEMULIB_VERSION_MAJOR, GNEISEMULIB_VERSION_MINOR)
 	);
 	EXPECT_TRUE(
-			gneis::info::Version::Matches(GNEISEMULIB_VERSION_MAJOR)
+			Version::Matches(GNEISEMULIB_VERSION_MAJOR)
 	);
 	EXPECT_FALSE(
-			gneis::info::Version::Matches(GNEISEMULIB_VERSION_MAJOR + 1, GNEISEMULIB_VERSION_MINOR, GNEISEMULIB_VERSION_PATCH)
+			Version::Matches(GNEISEMULIB_VERSION_MAJOR + 1, GNEISEMULIB_VERSION_MINOR, GNEISEMULIB_VERSION_PATCH)
 	);
 	EXPECT_FALSE(
-			gneis::info::Version::Matches(GNEISEMULIB_VERSION_MAJOR, GNEISEMULIB_VERSION_MINOR + 1, GNEISEMULIB_VERSION_PATCH)
+			Version::Matches(GNEISEMULIB_VERSION_MAJOR, GNEISEMULIB_VERSION_MINOR + 1, GNEISEMULIB_VERSION_PATCH)
 	);
 	EXPECT_FALSE(
-			gneis::info::Version::Matches(GNEISEMULIB_VERSION_MAJOR, GNEISEMULIB_VERSION_MINOR, GNEISEMULIB_VERSION_PATCH + 1)
+			Version::Matches(GNEISEMULIB_VERSION_MAJOR, GNEISEMULIB_VERSION_MINOR, GNEISEMULIB_VERSION_PATCH + 1)
 	);
+}
+
+}
+
 }
--- test/src/gneis/util/NameBuilderTest.cc
+++ test/src/gneis/util/NameBuilderTest.cc
@@ -1,26 +1,34 @@
 #include <gtest/gtest.h>
 #include "gneis/util/NameBuilder.hh"
 
-TEST(gneis_util_NameBuilder, Make_pchar_pchar)
+namespace gneis {
+
+namespace util {
+
+TEST(NameBuilder, Make_pchar_pchar)
 {
 	const char* const s1 = "first";
 	const char* const s2 = "second";
-	const G4String res = gneis::util::NameBuilder::Make(s1, s2);
+	const G4String res = NameBuilder::Make(s1, s2);
 	EXPECT_EQ("first:second", res);
 }
 
-TEST(gneis_util_NameBuilder, Make_g4s_pchar)
+TEST(NameBuilder, Make_g4s_pchar)
 {
 	const G4String s1 = "first";
 	const char* const s2 = "second";
-	const G4String res = gneis::util::NameBuilder::Make(s1, s2);
+	const G4String res = NameBuilder::Make(s1, s2);
 	EXPECT_EQ("first:second", res);
 }
 
-TEST(gneis_util_NameBuilder, Make_g4s_g4s)
+TEST(NameBuilder, Make_g4s_g4s)
 {
 	const G4String s1 = "first";
 	const G4String s2 = "second";
-	const G4String res = gneis::util::NameBuilder::Make(s1, s2);
+	const G4String res = NameBuilder::Make(s1, s2);
 	EXPECT_EQ("first:second", res);
+}
+
+}
+
 }
