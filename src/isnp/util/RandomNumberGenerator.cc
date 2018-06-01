9cef460f857a8d5bd9a8608c530a14a9ab9b8b9b
Andrey Nakin <andrey@ter-net.com>
2018-06-01 17:10:39 +0300

    work

--- src/isnp/util/RandomNumberGenerator.cc
+++ src/isnp/util/RandomNumberGenerator.cc
@@ -1,3 +1,5 @@
+#include <Randomize.hh>
+
 #include "isnp/util/RandomNumberGenerator.hh"
 
 namespace isnp {
@@ -5,7 +7,7 @@ namespace isnp {
 namespace util {
 
 G4double RandomNumberGenerator::locality(G4double const significant, unsigned const precision) {
-
+	return CLHEP::RandFlat::shoot(significant - 0.000005, significant + 0.000005);
 }
 
 }
--- test/src/isnp/util/RandomNumberGeneratorTest.cc
+++ test/src/isnp/util/RandomNumberGeneratorTest.cc
@@ -1,14 +1,35 @@
+#include <cmath>
+
 #include <gtest/gtest.h>
+
 #include "isnp/util/RandomNumberGenerator.hh"
+#include "isnp/testutil/Stat.hh"
 
 namespace isnp {
 
 namespace util {
 
-TEST(RandomNumberGenerator, locality)
+static void ScatteringTest(G4double const significant, unsigned const precision,
+		G4double const range) {
+
+	testutil::Stat v;
+
+	for (int i = 0; i < 1000000; i++) {
+		v += RandomNumberGenerator::locality(significant, precision);
+	}
+
+	EXPECT_TRUE(v.Is(significant));
+	EXPECT_NEAR(range / std::sqrt(12), v.GetStd(), range / 1000);
+
+}
+
+TEST(RandomNumberGenerator, LocalityScattering)
 {
-	// @TODO
-//	EXPECT_EQ("first:second", res);
+
+	ScatteringTest(1.23456, 6, 1e-5);
+	//ScatteringTest(1.23456e-5, 6, 1e-10);
+	//ScatteringTest(1.23456e5, 6, 1e-0);
+
 }
 
 }
