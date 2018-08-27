f8beab2395fe1e6857f7c03cac72b006f759d43f
Andrey Nakin <andrey@ter-net.com>
2018-08-27 14:04:17 +0300

    work

--- include/isnp/dist/AbstractDistribution.hh
+++ include/isnp/dist/AbstractDistribution.hh
@@ -2,30 +2,14 @@
 #define isnp_dist_AbstractDistribution_hh
 
 #include <G4ThreeVector.hh>
+#include "isnp/util/NonCopyable.hh"
 
 namespace isnp {
 
 namespace dist {
 
-class AbstractDistribution {
-
-	AbstractDistribution(AbstractDistribution&) = delete;
-	AbstractDistribution(const AbstractDistribution&) = delete;
-	AbstractDistribution(volatile AbstractDistribution&) = delete;
-	AbstractDistribution(const volatile AbstractDistribution&) = delete;
-
-	AbstractDistribution& operator=(AbstractDistribution&) = delete;
-	AbstractDistribution& operator=(const AbstractDistribution&) = delete;
-	AbstractDistribution& operator=(volatile AbstractDistribution&) = delete;
-	AbstractDistribution& operator=(const volatile AbstractDistribution&) = delete;
-
-protected:
-
-	AbstractDistribution();
-
+class AbstractDistribution: public util::NonCopyable {
 public:
-
-	virtual ~AbstractDistribution();
 
 	virtual G4ThreeVector Generate() const = 0;
 
---
+++ include/isnp/util/NonCopyable.hh
@@ -0,0 +1,34 @@
+#ifndef isnp_util_NonCopyable_hh
+#define isnp_util_NonCopyable_hh
+
+namespace isnp {
+
+namespace util {
+
+class NonCopyable {
+
+protected:
+
+	NonCopyable() = default;
+
+	NonCopyable(NonCopyable&) = delete;
+	NonCopyable(const NonCopyable&) = delete;
+	NonCopyable(volatile NonCopyable&) = delete;
+	NonCopyable(const volatile NonCopyable&) = delete;
+
+	NonCopyable& operator=(NonCopyable&) = delete;
+	NonCopyable& operator=(const NonCopyable&) = delete;
+	NonCopyable& operator=(volatile NonCopyable&) = delete;
+	NonCopyable& operator=(const volatile NonCopyable&) = delete;
+
+public:
+
+	virtual ~NonCopyable() = default;
+
+};
+
+}
+
+}
+
+#endif	//	isnp_util_NonCopyable_hh
