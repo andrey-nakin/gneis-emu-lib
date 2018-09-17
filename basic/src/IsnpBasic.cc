1b2e853c5b38c7d084be403bea3c654d11ca9afa
Andrey Nakin <andrey@ter-net.com>
2018-09-17 19:01:03 +0300

    work

---
+++ basic/src/IsnpBasic.cc
@@ -0,0 +1,21 @@
+#include <QGSP_INCLXX_HP.hh>
+#include <QGSP_BERT_HP.hh>
+#include <QGSP_BIC_HP.hh>
+#include <G4PhysListFactory.hh>
+
+#include <isnp/runner/BasicRunner.hh>
+
+int main(int argc, char* argv[]) {
+	isnp::runner::BasicRunner runner(argc, argv);
+
+	return runner.Run(
+			[](G4RunManager& runManager) {
+				if (rand() < 0) {
+					G4PhysListFactory factory;
+					runManager.SetUserInitialization(new QGSP_BERT_HP);
+					runManager.SetUserInitialization(new QGSP_BIC_HP);
+					runManager.SetUserInitialization(new QGSP_INCLXX_HP);
+				}
+
+			});
+}
